6517e877f48942b38046f746e7fb6da81715b254
vegchan <vegchan@gmail.com>
2020-01-10 00:00:45 +0530

    Added Angular Flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, FontAwesomeModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, FontAwesomeModule, FlexLayoutModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
